b60e8cb956ac461a655cb0f02454df366a0c6a87
Pavalo Carcheski <qfa.sistemas@gmail.com>
2023-10-12 16:02:00 -0400

    Inicialização

---
+++ src/main/java/br/com/qfa/QfaSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.qfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QfaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QfaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/qfa/QfaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.qfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QfaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
