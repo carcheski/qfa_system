cb416f0e56ca818f772be412fdc597cc41a66703
Pavalo Carcheski <qfa.sistemas@gmail.com>
2023-10-13 13:25:19 -0400

    Deletando uma categoria com Delete e tratamento de exception

--- src/main/java/br/com/qfa/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/qfa/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.com.qfa.services.exceptions.DataIntegrityExceptions;
 import br.com.qfa.services.exceptions.ObjectNotFoundExceptions;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -16,6 +17,13 @@ public class ResourceExceptionHandler {
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityExceptions.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityExceptions e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
 }
--- src/main/java/br/com/qfa/services/CategoriaService.java
+++ src/main/java/br/com/qfa/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.qfa.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.qfa.repositories.CategoriaRespository;
 import br.com.qfa.resources.domain.Categoria;
+import br.com.qfa.services.exceptions.DataIntegrityExceptions;
 import br.com.qfa.services.exceptions.ObjectNotFoundExceptions;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityExceptions("Não é possivel exluir uma categoria que possui produtos!!!");
+		}
 	}
 
 }
