28a33ee350b2aaebf4fedf3569164fe1e710331e
Pavalo Carcheski <qfa.sistemas@gmail.com>
2023-10-13 10:34:24 -0400

    Endpoint / pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/qfa/services/PedidoService.java
@@ -0,0 +1,24 @@
+package br.com.qfa.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.qfa.repositories.PedidoRespository;
+import br.com.qfa.resources.domain.Pedido;
+import br.com.qfa.services.exceptions.ObjectNotFoundExceptions;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRespository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundExceptions(
+				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
