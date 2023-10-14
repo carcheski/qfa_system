4ef2ac4e144678324717f54476c3ccf0ed16dda2
Pavalo Carcheski <qfa.sistemas@gmail.com>
2023-10-14 11:26:12 -0400

    PUT, DELETE e GET para Cliente:

--- src/main/java/br/com/qfa/services/ClienteService.java
+++ src/main/java/br/com/qfa/services/ClienteService.java
@@ -1,12 +1,18 @@
 package br.com.qfa.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import br.com.qfa.dto.ClienteDTO;
 import br.com.qfa.repositories.ClienteRespository;
 import br.com.qfa.resources.domain.Cliente;
+import br.com.qfa.services.exceptions.DataIntegrityExceptions;
 import br.com.qfa.services.exceptions.ObjectNotFoundExceptions;
 
 @Service
@@ -20,5 +26,39 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundExceptions(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityExceptions("Não é possivel exluir uma cliente que possui produtos!!!");
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
 
 }
